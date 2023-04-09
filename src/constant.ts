92df74693cd360053d6c943d62ac0034e646554a
havenquan <havenquan@qq.com>
2023-04-09 19:37:12 +0800

    1.0.9

--- src/constant.ts
+++ src/constant.ts
@@ -17,13 +17,13 @@ export function network(env?: string) {
     return {
       chainId: '0x1',
       chainName: 'ETH-Mainnet',
-      rpcUrls: ['https://eth-mainnet.g.alchemy.com/v2/Z_xFFXUwd05vfeJOS74UETGJVEd2A65Y']
+      rpcUrls: ['https://rpc.ankr.com/eth']
     };
   } else {
     return {
       chainId: '0x5',
       chainName: 'Goerli-Testnet',
-      rpcUrls: ['https://eth-goerli.g.alchemy.com/v2/dDCf0lfjfq1HeLlGAK3-u8r7hRhrJrEY']
+      rpcUrls: ['https://rpc.ankr.com/eth_goerli']
     };
   }
 };

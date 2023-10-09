a025bdd77c08414321081175f77f8e9ef54ca328
HavenCross <havenquan@qq.com>
2023-10-09 18:03:55 +0800

    24

--- src/pool/Guild.ts
+++ src/pool/Guild.ts
@@ -12,3 +12,40 @@ const client = new ApolloClient({
 export function guild() {
   return Guild__factory.connect(contract().Guild, rpcProvider);
 }
+
+export const otsInfo = {
+  getInviterInfos: async (
+    first: number,
+    skip: number,
+    orderBy: string,
+    orderDirection: string,
+    inviter?: string,
+    block?: number,
+  ) => {
+    const inviterInfosQuery = `
+      query($first: Int, $skip: Int, $orderBy: BigInt, $orderDirection: String, $inviter: String, $block: Int) {
+        inviterInfos(
+          first: $first, skip: $skip, orderBy: $orderBy, orderDirection: $orderDirection, ${block ? `block: { number: $block },` : ``}
+          where: {${inviter ? `inviter: $inviter,` : ``}}
+        ) {
+          inviter
+          usersLength
+          nftsLength
+          nftsPower
+        }
+      }
+    `;
+
+    return await client.query({
+      query: gql(inviterInfosQuery),
+      variables: {
+        first: first,
+        skip: skip,
+        orderBy: orderBy,
+        orderDirection: orderDirection,
+        block: block,
+        inviter: inviter,
+      },
+    });
+  },
+}
